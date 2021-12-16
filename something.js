1570b80bcc0989198d538123ddf453327b9c06e0
Yossi Saadi <yossisaadi@gmail.com>
2021-12-16 19:11:39 +0200

    Update something.js

--- something.js
+++ something.js
@@ -1,3 +1,14 @@
-const js = 'a';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
 
-const bla = '1';
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
